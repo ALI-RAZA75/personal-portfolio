01fc957452a1e2d7c01cdd7e6cf744f66bf54db8
ALI-RAZA75 <alirazaf75439@gmail.com>
2024-07-17 12:09:32 +0500

    porfolio add

---
+++ src/components/ContactLeft.tsx
@@ -0,0 +1,57 @@
+import { FaLinkedinIn } from "react-icons/fa";
+import { contactImg } from "../assets";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareGithub, faUpwork } from "@fortawesome/free-brands-svg-icons";
+
+const ContactLeft = () => {
+  return (
+    <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#0B1120] to-[#0B1120] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
+      <img
+        className="w-full h-64 object-cover rounded-lg mb-2"
+        src={contactImg}
+        alt="contactImg"
+      />
+      <div className="flex flex-col gap-4">
+        <h3 className="text-3xl font-bold text-white">John Doe</h3>
+        <p className="text-lg font-normal text-gray-400">
+          MERN Stack Developer
+        </p>
+        <p className="text-base text-gray-400 tracking-wide">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
+          ipsam autem cumque, accusantium dicta odio.
+        </p>
+        <p className="text-base text-gray-400 flex items-center gap-2">
+          Phone: <span className="text-lightText">+968 97859628</span>
+        </p>
+        <p className="text-base text-gray-400 flex items-center gap-2">
+          Email: <span className="text-lightText">noor.jsdivs@gmail.com</span>
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
+        <div className="flex gap-4">
+          <a href="https://github.com/ALI-RAZA75" target="_blank">
+            <span className="bannerIcon">
+            <FontAwesomeIcon icon={faSquareGithub} />
+            </span>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ali-raza-739724243/"
+            target="_blank"
+          >
+            <span className="bannerIcon">
+              <FaLinkedinIn />
+            </span>
+          </a>
+          <a href="https://www.upwork.com/freelancers/~01900a0003bfcb75d2" target="_blank">
+            <span className="bannerIcon">
+            <FontAwesomeIcon icon={faUpwork} />
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactLeft;
---
+++ src/components/LeftBanner.tsx
@@ -0,0 +1,83 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { FaLinkedinIn, FaReact } from "react-icons/fa";
+import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+import { FadeIn } from "./FadeIn";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareGithub, faUpwork } from "@fortawesome/free-brands-svg-icons";
+
+const LeftBanner = () => {
+  const [text] = useTypewriter({
+    words: ["Professional Coder.", "Full Stack Developer.", "UI Designer."],
+    loop: true,
+    typeSpeed: 20,
+    deleteSpeed: 10,
+    delaySpeed: 2000,
+  });
+  return (
+    <FadeIn className="w-full lgl:w-1/2 flex flex-col gap-20">
+      <div className="flex flex-col gap-5">
+        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h1 className="text-6xl font-bold text-white">
+          Hi, I'm <span className="text-designColor capitalize">Ali Raza</span>
+        </h1>
+        <h2 className="text-4xl font-bold text-white">
+          a <span>{text}</span>
+          <Cursor cursorStyle="|" cursorColor="#ff014f" />
+        </h2>
+        <p className="text-base font-bodyFont leading-6 tracking-wider">
+          I use animation as a third dimension by which to simplify experiences
+          and kuiding thro each and every interaction. I'm not adding motion
+          just to spruce things up, but doing it in ways that.
+        </p>
+      </div>
+      <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            Find me in
+          </h2>
+          <div className="flex gap-4">
+            <a href="https://www.upwork.com/freelancers/~01900a0003bfcb75d2" target="_blank">
+              <span className="bannerIcon">
+              <FontAwesomeIcon icon={faUpwork} />
+              </span>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/ali-raza-739724243/"
+              target="_blank"
+            >
+              <span className="bannerIcon">
+                <FaLinkedinIn />
+              </span>
+            </a>
+            <a href="https://github.com/ALI-RAZA75" target="_blank">
+              <span className="bannerIcon">
+              <FontAwesomeIcon icon={faSquareGithub} />
+              </span>
+            </a>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            BEST SKILL ON
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaReact />
+            </span>
+            <span className="bannerIcon">
+              <SiNextdotjs />
+            </span>
+            <span className="bannerIcon">
+              <SiTailwindcss />
+            </span>
+            <span className="bannerIcon">
+              <SiFigma />
+            </span>
+          </div>
+        </div>
+      </div>
+    </FadeIn>
+  );
+};
+
+export default LeftBanner;
